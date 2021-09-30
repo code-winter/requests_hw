ebd8b4f8b34eb67cabcd358e22fc8bed2ea95ac0
Dmitry Popov <ant1h4t3rz@gmail.com>
2021-09-30 17:35:32 +0900

    Added calls to every library, removed token for Yandex

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from stackoverflow import StackOverflow
 
 
 if __name__ == '__main__':
-    # Hulk = Heroes('Hulk')
-    # Cap_America = Heroes('Captain_America')
-    # Thanos = Heroes('Thanos')
-    # max_stats([Hulk.get_stats(), Cap_America.get_stats(), Thanos.get_stats()])
-    # token_yandex = 'AQAAAAAOgRTpAADLW9eaCRyTskHLg71bc-vNziw'
+    Hulk = Heroes('Hulk')
+    Cap_America = Heroes('Captain_America')
+    Thanos = Heroes('Thanos')
+    max_stats([Hulk.get_stats(), Cap_America.get_stats(), Thanos.get_stats()])
+    # token_yandex = ''
     # yan = Yandex(token_yandex)
     # remote_path = '/API_test/my_file.txt'
     # local_filename = 'my_file.txt'
     # yan.upload_to_disk(remote_path, local_filename)
-    s_over = StackOverflow()
-    res = s_over.get_response()
-    pprint(res)
+    stack = StackOverflow()
+    pprint(stack.get_response())
+
 
 
 
