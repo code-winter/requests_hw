f6f30b79aeaed0ce7f7b8cbb0f7d98d257884072
Dmitry Popov <ant1h4t3rz@gmail.com>
2021-09-30 20:40:13 +0900

    Finished logic for StackOverflow task

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import requests
 from heroes import Heroes, max_stats
 from pprint import pprint
 from yandex import Yandex
@@ -9,12 +8,12 @@ if __name__ == '__main__':
     Hulk = Heroes('Hulk')
     Cap_America = Heroes('Captain_America')
     Thanos = Heroes('Thanos')
-    # max_stats([Hulk.get_stats(), Cap_America.get_stats(), Thanos.get_stats()])
-    # token_yandex = ''
-    # yan = Yandex(token_yandex)
-    # remote_path = '/API_test/my_file.txt'
-    # local_filename = 'my_file.txt'
-    # yan.upload_to_disk(remote_path, local_filename)
+    max_stats([Hulk.get_stats(), Cap_America.get_stats(), Thanos.get_stats()])
+    token_yandex = ''
+    yan = Yandex(token_yandex)
+    remote_path = '/API_test/my_file.txt'
+    local_filename = 'my_file.txt'
+    yan.upload_to_disk(remote_path, local_filename)
     stack = StackOverflow()
     pprint(stack.get_questions(), sort_dicts=False)
 
