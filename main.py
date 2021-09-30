7176e76f0627974f8717cfc513d1fbf84f05cf13
Dmitry Popov <ant1h4t3rz@gmail.com>
2021-09-30 12:24:11 +0900

    Added code for Yandex API task

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import requests
 from heroes import Heroes, max_stats
 from pprint import pprint
+from yandex import Yandex
 
 
 if __name__ == '__main__':
-    Hulk = Heroes('Hulk')
-    Cap_America = Heroes('Captain_America')
-    Thanos = Heroes('Thanos')
-    max_stats([Hulk.get_stats(), Cap_America.get_stats(), Thanos.get_stats()])
+    # Hulk = Heroes('Hulk')
+    # Cap_America = Heroes('Captain_America')
+    # Thanos = Heroes('Thanos')
+    # max_stats([Hulk.get_stats(), Cap_America.get_stats(), Thanos.get_stats()])
+    # token_yandex = 'AQAAAAAOgRTpAADLW9eaCRyTskHLg71bc-vNziw'
+    # yan = Yandex(token_yandex)
+    # remote_path = '/API_test/my_file.txt'
+    # local_filename = 'my_file.txt'
+    # yan.upload_to_disk(remote_path, local_filename)
+
 
 
 
